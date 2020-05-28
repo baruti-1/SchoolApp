6e1ed6c6a324f084007d249743e38f91df7a0d9b
baruti-1 <barutibaruani@gmail.com>
2020-05-28 07:14:28 +0300

    Changed login activity

--- app/src/main/java/com/example/schoolapp/MainActivity.java
+++ app/src/main/java/com/example/schoolapp/MainActivity.java
@@ -18,14 +18,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginUser(View view){
-        username = (EditText)findViewById(R.id.editText);
-        password = (EditText)findViewById(R.id.editText2);
+        username = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
 
+        //Login implementation later (Hardcoded credentials for now)
         if(username.getText().toString().equals("admin") && password.getText().toString().equals("1234")){
             Intent login = new Intent(this, AdminActivity.class);
             startActivity(login);
+        }else if(username.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Please enter username and password",Toast.LENGTH_LONG).show();
         }else{
-            Toast.makeText(getApplicationContext(), "Invalid username/password",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Incorrect username or password",Toast.LENGTH_LONG).show();
         }
 
     }
